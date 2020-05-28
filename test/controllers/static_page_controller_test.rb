b0157768e839a4da9a6af4dd5f5a4ae1d435a0fe
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-05-28 16:23:28 +0000

    New branch!

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_page_home_url
     assert_response :success
+    assert_select "title" , "About-app"
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
+    assert_select "title" , "About-app"
+  end
+  
+  test "should get about" do
+    get static_page_about_url
+    assert_response :success
+    assert_select "title", "About-app"
   end
   
 end
