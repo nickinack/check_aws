1162ad1dcdc389d0faf9722141e628bbf48b80bf
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-05-29 17:25:35 +0000

    Added login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    if logged_in?
+      @user = User.find(params[:id])
+    else
+      redirect_to login_path
+    end
   end
   def new
     @user = User.new
   end
   def index
+    if !logged_in?
+      redirect_to login_path
+    end
+      
   end
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome ! You have been signed up!"
+      login @user
       redirect_to user_url(@user)
     else
       render 'new'
