f5a027c038c2a18cb41abebbb564b2c298ec0928
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-05-29 07:47:34 +0000

    Created Sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_page#about'
   get '/contact', to: 'static_page#contact'
   get '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_page#home'
 end
